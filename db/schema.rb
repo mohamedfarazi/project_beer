c95ec5c98eae1f9765cf5f762bae559375475c59
Natalie Jubb <natalie.jubb@gmail.com>
2014-03-04 17:31:29 -0500

    Added Plan model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140304201331) do
+ActiveRecord::Schema.define(version: 20140304222810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "plans", force: true do |t|
+    t.string   "name"
+    t.string   "freq"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",            null: false
