381d145336d8ab6fa45a5df1c16b6aebe91f3d2c
Natalie Jubb <natalie.jubb@gmail.com>
2014-03-05 14:30:54 -0500

    Added validations to the subscription model, errors to new subscription form

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,8 @@
 class Subscription < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :plan
+
+
+	validates :plan_id, presence: true
+	validates :n_packs,  numericality: { only_integer: true, greater_than_or_equal_to: 1 }
 end
