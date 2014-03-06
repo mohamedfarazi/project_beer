5583c2fbe53899568f504db837319dbbf0ed9611
Anojisho <anojisho@gmail.com>
2014-03-06 18:43:06 -0500

    delivery area validation warnings work

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ has_one :plan, through: :subscription
  def check_postal_code
  	allowed = ["M4X", "M4Y", "M5A", "M5B", "M5E", "M5J", "M5G", "M5S", "M5T", "M5V", "M6J", "M6K", "M6G", "M6H"]
      allowed.each do |p|
-     	return true if postal_code.include? p
+     	return true if postal_code.upcase.include? p
 
       end
       false
