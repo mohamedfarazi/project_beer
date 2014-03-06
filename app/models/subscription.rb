3fa97d0b241bbf95ea80e0cd93fa67fde88ee5fe
Natalie Jubb <natalie.jubb@gmail.com>
2014-03-06 13:33:03 -0500

    added upcoming deliveries helpers and view

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -5,4 +5,5 @@ class Subscription < ActiveRecord::Base
 
 	validates :plan_id, presence: true
 	validates :n_packs,  numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+	validates :first_delivery_date, presence: true
 end
