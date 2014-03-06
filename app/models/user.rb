e2b911a1f04e36342912948f8d617c15c47da0b6
Mohamed Farazi <mohamed.farazi@gmail.com>
2014-03-06 16:23:13 -0500

    Added an admin boolean for user classes that defaults to false

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
 
-validates :password, length: {minimum: 3}
-validates :password, confirmation: true
-validates :password_confirmation, presence: true
-validates :email, uniqueness: true
+	validates :password, length: {minimum: 3}
+	validates :password, confirmation: true
+	validates :password_confirmation, presence: true
+	validates :email, uniqueness: true
 
-has_one :subscription
-has_one :plan, through: :subscription
+	has_one :subscription
+	has_one :plan, through: :subscription
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140306175105) do
+ActiveRecord::Schema.define(version: 20140306202212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,9 +66,9 @@ ActiveRecord::Schema.define(version: 20140306175105) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                       null: false
-    t.string   "crypted_password",            null: false
-    t.string   "salt",                        null: false
+    t.string   "email",                                       null: false
+    t.string   "crypted_password",                            null: false
+    t.string   "salt",                                        null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "activation_state"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 20140306175105) do
     t.string   "phone"
     t.string   "contact_name"
     t.string   "company_name"
+    t.boolean  "admin",                       default: false
   end
 
   add_index "users", ["activation_token"], name: "index_users_on_activation_token", using: :btree
