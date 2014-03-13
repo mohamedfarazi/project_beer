c01b4b6c7d7b19853040b2049f5ae7d206ebbf62
Mohamed Farazi <mohamed.farazi@gmail.com>
2014-03-13 15:14:46 -0400

    Added a gMaps div so we can show customers our different delivery range

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -19,5 +19,42 @@ $(document).ready(function(){
 	// setTimeout( function() {
 	// 	cbpFixedScrollLayout.init();
 	// }, 5000);
-
 });
+
+jQuery(document).ready(function($) {
+	function initialize() {
+	  var mapDiv = document.getElementById('map-canvas');
+	  var map = new google.maps.Map(mapDiv, {
+	    center: new google.maps.LatLng(43.652481,-79.379236),
+	    zoom: 13,
+	    mapTypeId: google.maps.MapTypeId.ROADMAP,
+	    disableDefaultUI: true,
+	    scrollwheel: false,
+	     navigationControl: false,
+	     mapTypeControl: false,
+	     scaleControl: false,
+	     draggable: false,
+	     setClickable: false
+	  });
+	  var paths = [new google.maps.LatLng(43.659886,-79.435332),
+	    new google.maps.LatLng(43.630085, -79.424201),
+	    new google.maps.LatLng(43.651483, -79.347189),
+	    new google.maps.LatLng(43.67559, -79.361716),
+	    new google.maps.LatLng(43.659886,-79.435332)];
+
+	  var shape = new google.maps.Polygon({
+       paths: paths,
+       strokeColor: '#ff9900',
+       strokeOpacity: 0.8,
+       strokeWeight: 2,
+       fillColor: '#ff9900',
+       fillOpacity: 0.35
+       });
+
+       shape.setMap(map);
+}
+
+        google.maps.event.addDomListener(window, 'load', initialize);
+});
+
+///Need to disable zoom still.
